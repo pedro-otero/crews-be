a7d8ab6e3594e432441a83bf7ae04fbf35c71651
Pedro Otero <pedro.otero.prada@gmail.com>
2018-03-17 23:26:01 -0500

    Add reducers and store
    (cherry picked from commit e6fc52b)

---
+++ routes/album/state/reducers.js
@@ -0,0 +1,15 @@
+const { ADD_SEARCH } = require('./actions');
+const { combineReducers } = require('redux');
+
+const searches = (state = {}, { type, id }) => {
+  switch (type) {
+    case ADD_SEARCH:
+      const newSearch = { matches: [] };
+      return Object.assign({
+        [id]: newSearch
+      }, ...state);
+  }
+  return state;
+};
+
+module.exports = combineReducers({ searches });
