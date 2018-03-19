1a5bdd4361610c49f5bd58a1adfd06d1d58312d7
Pedro Otero <pedro.otero.prada@gmail.com>
2018-03-18 21:52:02 -0500

    Answer the request immediately

--- routes/album/index.js
+++ routes/album/index.js
@@ -9,6 +9,7 @@ const matchAlbum = require('./match');
 
 router.get('/:spotifyAlbumId', function (req, res) {
 
+
   const discogify = req.app.locals.discogify;
   const spotifyApi = req.app.locals.spotifyApi;
 
@@ -17,25 +18,30 @@ router.get('/:spotifyAlbumId', function (req, res) {
     res.status(500).send(e.stack)
   }
 
-  spotifyApi
-    .then(api => {
-      const albumId = req.params.spotifyAlbumId;
-      store.dispatch(actions.addSearch(albumId));
-      return api.getAlbum(albumId);
-    })
-    .then(response => {
-      const album = response.body;
-      store.dispatch(actions.addAlbum(album));
-      return album;
-    })
-    .then(album => {
-      discogify.findReleases(album).then(releases => {
-        store.dispatch(actions.addMatches(album.id, releases));
-        const release = matchAlbum(album, releases);
-        const builtAlbum = buildAlbum(album, release);
-        res.json(builtAlbum);
+  const search = store.getState().searches.find(item => item.id === req.params.spotifyAlbumId);
+
+  if (!search) {
+    spotifyApi
+      .then(api => {
+        const albumId = req.params.spotifyAlbumId;
+        store.dispatch(actions.addSearch(albumId));
+        return api.getAlbum(albumId);
+      })
+      .then(response => {
+        const album = response.body;
+        store.dispatch(actions.addAlbum(album));
+        return album;
+      })
+      .then(album => {
+        discogify.findReleases(album).then(releases => {
+          store.dispatch(actions.addMatches(album.id, releases));
+          const release = matchAlbum(album, releases);
+          const builtAlbum = buildAlbum(album, release);
+        }).catch(doCatch);
       }).catch(doCatch);
-    }).catch(doCatch);
+  }
+
+  res.json(search);
 });
 
 module.exports = router;
