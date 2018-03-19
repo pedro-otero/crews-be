b045c75558e1297f3176d27589216ba675200c11
Pedro Otero <pedro.otero.prada@gmail.com>
2018-03-18 20:51:30 -0500

    Switch searches data struct from object into array

--- routes/album/state/reducers.js
+++ routes/album/state/reducers.js
@@ -1,13 +1,10 @@
 const { ADD_SEARCH, ADD_ALBUM } = require('./actions');
 const { combineReducers } = require('redux');
 
-const searches = (state = {}, { type, id }) => {
+const searches = (state = [], { type, id }) => {
   switch (type) {
     case ADD_SEARCH:
-      const newSearch = { matches: [] };
-      return Object.assign({
-        [id]: newSearch
-      }, state);
+      return [{ id, matches: [] }, ...state];
   }
   return state;
 };
