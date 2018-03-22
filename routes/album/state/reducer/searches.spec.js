33dc8e0c8b96e32402acbf102c1abb868af40f4f
Pedro Otero <pedro.otero.prada@gmail.com>
2018-03-21 22:51:43 -0500

    Refactor

--- routes/album/state/reducer/searches.spec.js
+++ routes/album/state/reducer/searches.spec.js
@@ -10,7 +10,7 @@ const reduce = require('./searches');
 describe('Searches reducer', function () {
 
   const addSearch = id => function () {
-    this.newSearches = reduce([], {
+    this.searches = reduce([], {
       type: ADD_SEARCH,
       id
     });
@@ -20,21 +20,21 @@ describe('Searches reducer', function () {
     beforeEach(addSearch('albumId'));
 
     it('Pushes searches', function () {
-      assert.equal(1, this.newSearches.length);
+      assert.equal(1, this.searches.length);
     });
 
     it('Creates a request with the same id as the album', function () {
-      assert.equal('albumId', this.newSearches[0].id);
+      assert.equal('albumId', this.searches[0].id);
     });
 
     it('Creates a request with status ADDED', function () {
-      assert.equal('ADDED', this.newSearches[0].status);
+      assert.equal('ADDED', this.searches[0].status);
     });
   });
 
   describe(ADD_MATCHES, function () {
     beforeEach(function () {
-      this.newSearches = reduce([{
+      this.searches = reduce([{
         id: 'albumId',
       }], {
         type: ADD_MATCHES,
@@ -52,32 +52,32 @@ describe('Searches reducer', function () {
     });
 
     it('puts one match in the search', function () {
-      assert.equal(1, this.newSearches[0].matches.length);
+      assert.equal(1, this.searches[0].matches.length);
     });
 
     it('matches release to the search by album id', function () {
-      assert.equal(1, this.newSearches[0].matches[0]);
+      assert.equal(1, this.searches[0].matches[0]);
     });
 
     it('sets search as MATCHED', function () {
-      assert.equal('MATCHED', this.newSearches[0].status);
+      assert.equal('MATCHED', this.searches[0].status);
     });
 
     it('sets search\s builtAlbum', function () {
-      assert(this.newSearches[0].builtAlbum);
+      assert(this.searches[0].builtAlbum);
     });
   });
 
   describe(SET_STATUS, function () {
     it('sets the status', function () {
-      const newSearches = reduce([{
+      const searches = reduce([{
         id: 'albumId',
       }], {
         type: SET_STATUS,
         id: 'albumId',
         status: 'some status'
       });
-      assert.equal('some status', newSearches[0].status);
+      assert.equal('some status', searches[0].status);
     });
   });
 });
