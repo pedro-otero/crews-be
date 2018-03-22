bfbc889eff669128439ae8da113b910c64973093
Pedro Otero <pedro.otero.prada@gmail.com>
2018-03-21 20:49:03 -0500

    Extract searches reducer

--- routes/album/state/reducers/index.js
+++ routes/album/state/reducers/index.js
@@ -1,39 +1,5 @@
-const { ADD_SEARCH, ADD_ALBUM, ADD_MATCHES, SET_STATUS, RESULTS } = require('../actions');
-
-const buildAlbum = require('../../build');
-const matchAlbum = require('../../match');
-
-const idFilter = id => item => item.id === id;
-
-const searches = (state = [], { type, id, releases, album, status, entity, results }) => {
-  let search, newSearch;
-  switch (type) {
-    case ADD_SEARCH:
-      newSearch = { id, status: 'ADDED' };
-      break;
-    case ADD_MATCHES:
-      const release = matchAlbum(album, releases);
-      const builtAlbum = buildAlbum(album, release);
-      search = state.find(idFilter(album.id));
-      newSearch = Object.assign({}, search, {
-        status: 'MATCHED',
-        matches: [releases.map(release => release.id), ...(search.matches || [])],
-        builtAlbum
-      });
-      break;
-    case SET_STATUS:
-      search = state.find(idFilter(id));
-      newSearch = Object.assign({}, search, { status });
-      break;
-    case RESULTS:
-      search = state.find(idFilter(id));
-      newSearch = Object.assign({}, search, { [`${entity}Results`]: results });
-      break;
-    default:
-      return state;
-  }
-  return [newSearch, ...state.filter(idFilter(id))];
-};
+const { ADD_ALBUM, ADD_MATCHES } = require('../actions');
+const searches = require('./searches');
 
 const albums = (state = [], { type, album }) => {
   switch (type) {
@@ -41,7 +7,7 @@ const albums = (state = [], { type, album }) => {
       return [album, ...state];
   }
   return state;
-}
+};
 
 const releases = (state = [], { type, releases }) => {
   switch (type) {
@@ -49,6 +15,6 @@ const releases = (state = [], { type, releases }) => {
       return [...releases, ...state];
   }
   return state;
-}
+};
 
 module.exports = ({ searches, albums, releases });
---
+++ routes/album/state/reducers/searches.js
@@ -0,0 +1,36 @@
+const { ADD_SEARCH, ADD_MATCHES, SET_STATUS, RESULTS } = require('../actions');
+
+const buildAlbum = require('../../build');
+const matchAlbum = require('../../match');
+
+const idFilter = id => item => item.id === id;
+
+module.exports = (state = [], { type, id, releases, album, status, entity, results }) => {
+  let search, newSearch;
+  switch (type) {
+    case ADD_SEARCH:
+      newSearch = { id, status: 'ADDED' };
+      break;
+    case ADD_MATCHES:
+      const release = matchAlbum(album, releases);
+      const builtAlbum = buildAlbum(album, release);
+      search = state.find(idFilter(album.id));
+      newSearch = Object.assign({}, search, {
+        status: 'MATCHED',
+        matches: [releases.map(release => release.id), ...(search.matches || [])],
+        builtAlbum
+      });
+      break;
+    case SET_STATUS:
+      search = state.find(idFilter(id));
+      newSearch = Object.assign({}, search, { status });
+      break;
+    case RESULTS:
+      search = state.find(idFilter(id));
+      newSearch = Object.assign({}, search, { [`${entity}Results`]: results });
+      break;
+    default:
+      return state;
+  }
+  return [newSearch, ...state.filter(idFilter(id))];
+};
