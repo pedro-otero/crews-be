d5f874ca204cd57dda5cd05c0569136491e134f7
Pedro Otero <pedro.otero.prada@gmail.com>
2018-03-21 22:18:54 -0500

    Put action in its own describe

--- routes/album/state/reducer/searches.spec.js
+++ routes/album/state/reducer/searches.spec.js
@@ -1,25 +1,27 @@
 const assert = require('assert');
 
-const actions = require('../action/constants');
+const { ADD_SEARCH } = require('../action/constants');
 const searches = require('./searches');
 
 describe('Searches reducer', function () {
-  beforeEach(function () {
-    this.newSearches = searches([], {
-      type: actions.ADD_SEARCH,
-      id: 'albumId'
+  describe(ADD_SEARCH, function () {
+    beforeEach(function () {
+      this.newSearches = searches([], {
+        type: ADD_SEARCH,
+        id: 'albumId'
+      });
     });
-  });
 
-  it('Pushes searches', function () {
-    assert.equal(1, this.newSearches.length);
-  });
+    it('Pushes searches', function () {
+      assert.equal(1, this.newSearches.length);
+    });
 
-  it('Creates a request with the same id as the album', function () {
-    assert.equal('albumId', this.newSearches[0].id);
-  });
+    it('Creates a request with the same id as the album', function () {
+      assert.equal('albumId', this.newSearches[0].id);
+    });
 
-  it('Creates a request with status ADDED', function () {
-    assert.equal('ADDED', this.newSearches[0].status);
+    it('Creates a request with status ADDED', function () {
+      assert.equal('ADDED', this.newSearches[0].status);
+    });
   });
 });
