1075ba4bc0e02439ba2c3b55de0f2be219821805
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-03 17:30:32 -0400

    Add combined search results reducer to root reducer

--- routes/album/state/reducer/index.js
+++ routes/album/state/reducer/index.js
@@ -1,5 +1,6 @@
 const searches = require('./searches');
 const albums = require('./albums');
 const releases = require('./releases');
+const results = require('./results');
 
-module.exports = ({ searches, albums, releases });
+module.exports = ({ searches, albums, releases, results });
