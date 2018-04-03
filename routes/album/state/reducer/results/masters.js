2a5009e3ab0f1f76a470cf6fdf6271cc0fdee921
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-03 16:37:49 -0400

    Add master search results action

--- routes/album/state/reducer/results/masters.js
+++ routes/album/state/reducer/results/masters.js
@@ -1,3 +1,14 @@
+const { ADD_MASTER_RESULTS } = require("../../action/constants");
+
 module.exports = function (state = [], action) {
+  const func = {
+    [ADD_MASTER_RESULTS]: () => [...state, {
+      album: action.album,
+      results: action.results,
+    }]
+  }[action.type];
+  if (func) {
+    return func();
+  }
   return state;
 };
--- routes/album/state/reducer/results/masters.spec.js
+++ routes/album/state/reducer/results/masters.spec.js
@@ -1,10 +1,24 @@
 const assert = require('assert');
 
 const reduce = require('./masters');
+const {
+  ADD_MASTER_RESULTS
+} = require('../../action/constants');
 
 describe('Master search results reducer', () => {
   it('returns default state', () => {
     const results = reduce(undefined, {});
     assert.equal(0, results.length);
+  });
+
+  it('stores results for an album', () => {
+    const results = reduce([], {
+      type: ADD_MASTER_RESULTS,
+      action: {
+        album: 1,
+        results: [{}],
+      }
+    });
+    assert.equal(1, results.length);
   });
 });
