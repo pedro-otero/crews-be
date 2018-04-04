9ca631ec929b9deb6d77f4a375f1edfc0dc97d8e
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-04 19:27:59 -0400

    Create params function

--- routes/album/search/index.js
+++ routes/album/search/index.js
@@ -5,19 +5,21 @@ const { actions } = require('../state');
 
 module.exports = function (db) {
 
-  const find = (album, type) => {
-    const { id, artists, name } = album;
-    let page = 1;
-    return db.search({
-      artist: artists[0].name,
+  const params = ({ artists: [{ name: artist }], name }, type, page = 1) => {
+    return {
+      artist,
       release_title: name.replace(/(.+) \((.+)\)/, '$1'),
       type,
       per_page: 100,
       page,
-    }).then(page => {
+    };
+  };
+
+  const find = (album, type) => {
+    return db.search(params(album, type)).then(page => {
       ({
-        'master': () => actions.masterResults(id, page),
-        'release': () => actions.releaseResults(id, page),
+        'master': () => actions.masterResults(album.id, page),
+        'release': () => actions.releaseResults(album.id, page),
       })[type]();
       const first = match(album).by('title', 'exact title', 'format', 'year')(page.results);
       const second = page.results.filter(result => !first.find(f => f.id === result.id));
