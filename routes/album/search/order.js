831ffc37eb58b569fb19253956a7f1485e768094
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-04 20:16:58 -0400

    Match by only release title

--- routes/album/search/order.js
+++ routes/album/search/order.js
@@ -1,10 +1,24 @@
 module.exports = (results, { name, artists: [{ name: artist }] }) => {
-  return results.reduce((ordered, release) => {
+  return results.map((release, position) => {
     const exactTitle = `${artist} - ${name}`;
+    let score = 0;
     if (exactTitle === release.title) {
-      return [release].concat(ordered);
-    } else {
-      return ordered.concat([release]);
+      score += 25;
     }
-  });
+    if (release.title.match(`.+ - ${name.replace(/(.+) \((.+)\)/, '$1').toUpperCase()}`)) {
+      score += 25;
+    }
+    return { position, score };
+  }).reduce((ordered, item) => {
+    if (ordered.length) {
+      const position = ordered.findIndex(innerItem => innerItem.score < item.score);
+      return [
+        ...ordered.slice(0, position),
+        item,
+        ...ordered.slice(position, ordered.length)
+      ]
+    } else {
+      return [item];
+    }
+  }, []).map(item => results[item.position]);
 };
--- routes/album/search/order.spec.js
+++ routes/album/search/order.spec.js
@@ -29,4 +29,31 @@ describe('Order search results function', function () {
       assert.equal(2, this.ordered.length);
     });
   });
+
+  describe('orders by release title', function () {
+    beforeEach(function () {
+      this.ordered = order([{
+        id: 1,
+        title: 'Artist - Other Album'
+      }, {
+        id: 2,
+        title: 'Artist - Album'
+      }], {
+        name: 'Album',
+        artists: [{ name: 'Artist' }]
+      });
+    });
+
+    it('first item is release with id 2', function () {
+      assert.equal(2, this.ordered[0].id);
+    });
+
+    it('second item is release with id 1', function () {
+      assert.equal(1, this.ordered[1].id);
+    });
+
+    it('preserves array length', function () {
+      assert.equal(2, this.ordered.length);
+    });
+  });
 });
