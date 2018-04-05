5345482a8736d6a72cf7b88860e4ea3920dac0ea
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-05 19:26:17 -0400

    Refactor

--- routes/album/view/query.js
+++ routes/album/view/query.js
@@ -1,12 +1,14 @@
 module.exports = function (albumId, store) {
+  const state = () => store.getState();
+
   return {
     getMasterSearchResults: () => {
-      return store.getState().results.masters
+      return state().results.masters
         .filter(result => result.album === albumId)
         .reduce((all, item) => all.concat(item.page.results), []);
     },
     getReleaseSearchResults: () => {
-      return store.getState().results.releases
+      return state().results.releases
         .filter(result => result.album === albumId)
         .reduce((all, item) => all.concat(item.page.results), []);
     },
