dd62e13d8ef7e0003a04f3ef57e8e141930bd280
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-05 21:21:10 -0400

    Make tests independent

--- routes/album/view/query.spec.js
+++ routes/album/view/query.spec.js
@@ -3,25 +3,14 @@ const assert = require('assert');
 const Query = require('./query');
 
 describe('Search state view', function () {
-  beforeEach(function () {
-    this.store = {
+  it('Gets master search results', function () {
+    const store = {
       getState: () => ({
         results: {
           masters: [{
             album: 1,
             page: {
-              results: [{ id: 'a' }, {}, { id: 'c' },]
-            }
-          }, {
-            album: 1,
-            page: {
-              results: [{}, {},]
-            }
-          },],
-          releases: [{
-            album: 1,
-            page: {
-              results: [{}]
+              results: [{}, {}, {},]
             }
           }, {
             album: 1,
@@ -35,16 +24,30 @@ describe('Search state view', function () {
           { id: 12, master_id: 'c' }, { id: 13, master_id: 'd' },]
       })
     };
-  });
-
-  it('Gets master search results', function () {
-    const query = Query(1, this.store);
+    const query = Query(1, store);
     const masterSearchResults = query.getMasterSearchResults();
     assert.equal(5, masterSearchResults.length);
   });
 
   it('Gets release search results', function () {
-    const query = Query(1, this.store);
+    const store = {
+      getState: () => ({
+        results: {
+          releases: [{
+            album: 1,
+            page: {
+              results: [{}]
+            }
+          }, {
+            album: 1,
+            page: {
+              results: [{}, {},]
+            }
+          },]
+        }
+      })
+    };
+    const query = Query(1, store);
     const releaseSearchResults = query.getReleaseSearchResults();
     assert.equal(3, releaseSearchResults.length);
   });
