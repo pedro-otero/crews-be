68dcaeb1fe19b93f4c8e304907f02d9721282327
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-06 17:10:53 -0400

    Refactor

--- src/setup/discogs.js
+++ src/setup/discogs.js
@@ -1,10 +1,10 @@
 const Throxy = require('throxy');
 const Disconnect = require('disconnect');
 
-const Search = require('../discogs');
+const DiscogsFinder = require('../discogs');
 const disconnectConfig = require('../../disconnect-config.json');
 
 const disconnect = new Disconnect.Client(disconnectConfig.agent, disconnectConfig.keys);
-const throxy = new Throxy(disconnect.database(), 1100);
+const throttledDatabase = new Throxy(disconnect.database(), 1100);
 
-module.exports = new Search(throxy);
+module.exports = new DiscogsFinder(throttledDatabase);
