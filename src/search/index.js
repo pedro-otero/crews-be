1c87e79733e6fb789a6c93ed188a854a5ed14cd1
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-06 10:43:27 -0400

    Search only releases

--- src/search/index.js
+++ src/search/index.js
@@ -5,42 +5,20 @@ const { actions } = require('../redux/state/index');
 
 module.exports = function (db) {
 
-  const params = ({ artists: [{ name: artist }], name }, type, page = 1) => ({
-    artist,
-    release_title: name.replace(/(.+) \((.+)\)/, '$1'),
-    type,
-    per_page: 100,
-    page,
-  });
-
-  const find = (album, type) => {
-    return db.search(params(album, type)).then(page => {
-      ({
-        'master': () => actions.masterResults(album.id, page),
-        'release': () => actions.releaseResults(album.id, page),
-      })[type]();
-      return order(page.results, album);
-    });
-  };
-
   this.findReleases = album => {
-    return find(album, 'master')
-      .then(masters => {
-        if (masters.length) {
-          masters.forEach(master => {
-            db.getMasterVersions(master.id).then(result => {
-              result.versions.forEach(version => {
-                db.getRelease(version.id).then(actions.addRelease);
-              });
-            });
-          });
-        } else {
-          find(album, 'release').then(results => {
-            results.forEach(result => {
-              db.getRelease(result.id).then(actions.addRelease);
-            })
-          });
-        }
-      });
+    const { artists: [{ name: artist }], name } = album;
+    return db.search({
+      artist,
+      release_title: name.replace(/(.+) \((.+)\)/, '$1'),
+      type: 'release',
+      per_page: 100,
+      page: 1,
+    }).then(page => {
+      actions.releaseResults(album.id, page);
+      const results = order(page.results, album);
+      results.forEach(result => {
+        db.getRelease(result.id).then(actions.addRelease);
+      })
+    });
   }
 };
