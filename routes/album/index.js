cde56384e406d1bfe7e01185f86bb5042af45e82
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-06 17:35:22 -0400

    Move all non express stuff out of route

--- routes/album/index.js
+++ routes/album/index.js
@@ -1,28 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { store } = require('../../src/redux/state');
-const Query = require('../../src/redux/view/query');
 const searchAlbum = require('../../src/search');
 
 router.get('/:spotifyAlbumId', function (req, res) {
-
   const { discogs, spotify } = req.app.locals;
   const { spotifyAlbumId } = req.params;
-
-  const search = store.getState().searches.find(item => item.id === spotifyAlbumId);
-
-  if (search) {
-    const query = Query(spotifyAlbumId, store);
-    res.json(Object.assign(search, {
-      bestMatch: query.getBestMatch(),
-      built: query.getAllMatches(),
-    }));
-  } else {
-    searchAlbum(spotify, spotifyAlbumId, discogs);
-    res.status(201).json({ id: spotifyAlbumId, status: 'CREATED' });
-  }
-  return;
+  const { status, data } = searchAlbum(spotify, spotifyAlbumId, discogs);
+  res.status(status).json(data);
 });
 
 module.exports = router;
--- src/search/index.js
+++ src/search/index.js
@@ -1,24 +1,45 @@
-const { actions } = require('../redux/state');
+const { store, actions } = require('../redux/state');
+const Query = require('../redux/view/query');
 
-module.exports = function (spotify, spotifyAlbumId, discogs) {
+module.exports = function searchAlbum(spotify, spotifyAlbumId, discogs) {
 
   const doCatch = e => {
     console.log(e);
     return;
   };
 
-  spotify
-    .then(api => {
-      actions.addSearch(spotifyAlbumId);
-      return api.getAlbum(spotifyAlbumId);
-    })
-    .then(response => {
-      const album = response.body;
-      actions.addAlbum(album);
-      return album;
-    })
-    .then(album => {
-      discogs.findReleases(album).catch(doCatch);
-    })
-    .catch(doCatch);
+  const search = store.getState().searches.find(item => item.id === spotifyAlbumId);
+
+  if (search) {
+    const query = Query(spotifyAlbumId, store);
+    return Object.assign(search, {
+      data: {
+        bestMatch: query.getBestMatch(),
+        built: query.getAllMatches(),
+      },
+      status: 200
+    });
+  } else {
+    spotify
+      .then(api => {
+        actions.addSearch(spotifyAlbumId);
+        return api.getAlbum(spotifyAlbumId);
+      })
+      .then(response => {
+        const album = response.body;
+        actions.addAlbum(album);
+        return album;
+      })
+      .then(album => {
+        discogs.findReleases(album).catch(doCatch);
+      })
+      .catch(doCatch);
+    return {
+      data: {
+        id: spotifyAlbumId,
+        status: 'CREATED'
+      },
+      status: 201
+    };
+  }
 };
