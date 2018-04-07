1e35b1b393a02a0cced38c083e8ea30ff6236499
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-06 23:56:00 -0400

    Lint

--- src/redux/view/query.spec.js
+++ src/redux/view/query.spec.js
@@ -2,50 +2,50 @@ const assert = require('assert');
 
 const Query = require('./query');
 
-describe('Search state view', function () {
-  it('Gets release search results', function () {
+describe('Search state view', () => {
+  it('Gets release search results', () => {
     const store = {
       getState: () => ({
         results: [{
           album: 1,
           page: {
-            results: [{}]
-          }
+            results: [{}],
+          },
         }, {
           album: 1,
           page: {
-            results: [{}, {},]
-          }
-        },]
-      })
+            results: [{}, {}],
+          },
+        }],
+      }),
     };
     const query = Query(1, store);
     const releaseSearchResults = query.getReleaseSearchResults();
     assert.equal(3, releaseSearchResults.length);
   });
 
-  it('Gets retrieved releases', function () {
+  it('Gets retrieved releases', () => {
     const store = {
       getState: () => ({
         results: [{
           album: 1,
           page: {
-            results: [{ id: 'r1' }, { id: 'r2' }]
-          }
-        },],
-        releases: [{ id: 'r1' }, { id: 'r2' },]
-      })
+            results: [{ id: 'r1' }, { id: 'r2' }],
+          },
+        }],
+        releases: [{ id: 'r1' }, { id: 'r2' }],
+      }),
     };
     const query = Query(1, store);
     const releases = query.getRetrievedReleases();
     assert.equal(2, releases.length);
   });
 
-  it('Gets album', function () {
+  it('Gets album', () => {
     const store = {
       getState: () => ({
-        albums: [{ id: 1 }]
-      })
+        albums: [{ id: 1 }],
+      }),
     };
     const query = Query(1, store);
     const album = query.getAlbum();
