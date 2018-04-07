d9a1b82b4ba5318f2ddaac29173c1c5bc8988ff8
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-06 23:54:34 -0400

    Lint

--- src/redux/view/comparators/tracklist.js
+++ src/redux/view/comparators/tracklist.js
@@ -1,7 +1,6 @@
 const similarity = require('string-similarity').compareTwoStrings;
 
 module.exports = (spotify, discogs) => {
-
   if (discogs.length !== spotify.length) {
     return false;
   }
@@ -9,5 +8,5 @@ module.exports = (spotify, discogs) => {
   return spotify
     .map(track => track.name)
     .map((track, i) => similarity(track, discogs[i].title))
-    .reduce((sum, similarity, i, array) => sum + (similarity / array.length), 0);
+    .reduce((sum, current, i, array) => sum + (current / array.length), 0);
 };
