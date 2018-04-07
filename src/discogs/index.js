6800d3fb62ee88303b1415611f9693cc8b84baa3
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-06 22:42:24 -0400

    Refine logging and load all pages

--- src/discogs/index.js
+++ src/discogs/index.js
@@ -32,13 +32,17 @@ module.exports = function (db) {
     };
 
     const loadAllReleases = page => {
-      logger.info(msg(album, `${page.pagination.items} release results found`));
+      logger.info(msg(album, `${page.pagination.page}/${page.pagination.pages} page with ${page.results.length} release results found`));
       actions.releaseResults(album.id, page);
       const results = order(page.results, album);
-      results.forEach(result => {
+      results.forEach((result, i) => {
         db.getRelease(result.id).then(release => {
-          logger.info(msg(album, `Release ${release.id} (master ${release.master_id}) retrieved`));
+          logger.info(msg(album, `P(${page.pagination.page}/${page.pagination.pages}) R(${(i + 1)}/${results.length}) Release ${release.id} (master ${release.master_id}) retrieved`));
           actions.addRelease(release);
+          if (i === page.results.length - 1 && page.pagination.page < page.pagination.pages) {
+            Object.assign(params, { page: page.pagination.page + 1 });
+            db.search(params).then(loadAllReleases).catch(doCatch);
+          }
         }).catch(doCatch);
       });
     };
