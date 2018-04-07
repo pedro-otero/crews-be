cdae9bfcf0f18cdfb8b31709f69562d31a16682b
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-06 23:45:51 -0400

    Lint

--- src/redux/state/reducer/results.js
+++ src/redux/state/reducer/results.js
@@ -1,11 +1,11 @@
-const { ADD_RELEASE_RESULTS } = require("../action/constants");
+const { ADD_RELEASE_RESULTS } = require('../action/constants');
 
 module.exports = function (state = [], action) {
   const func = {
     [ADD_RELEASE_RESULTS]: () => [...state, {
       album: action.album,
       page: action.page,
-    }]
+    }],
   }[action.type];
   if (func) {
     return func();
