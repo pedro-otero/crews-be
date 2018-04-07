7b3c86eaa1cbf80ed9de83d141dbb0561ad55706
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-06 23:45:35 -0400

    Lint

--- src/discogs/index.js
+++ src/discogs/index.js
@@ -1,26 +1,23 @@
-"use strict";
-
 const winston = require('winston');
 
 const order = require('./order');
 const { actions } = require('../redux/state/index');
 
 module.exports = function (db) {
-  const doCatch = e => {
+  const doCatch = (e) => {
     console.log(e);
-    return;
   };
 
-  this.findReleases = album => {
+  this.findReleases = (album) => {
     const logger = winston.createLogger({
       level: 'info',
       format: winston.format.json(),
       transports: [
-        new winston.transports.Console()
-      ]
+        new winston.transports.Console(),
+      ],
     });
 
-    const msg = (album, theRest) => `${album.artists[0].name} - ${album.name} (${album.id}) :: ${theRest}`;
+    const msg = text => `${album.artists[0].name} - ${album.name} (${album.id}) :: ${text}`;
 
     const { artists: [{ name: artist }], name } = album;
     const params = {
@@ -31,12 +28,12 @@ module.exports = function (db) {
       page: 1,
     };
 
-    const loadAllReleases = page => {
+    const loadAllReleases = (page) => {
       logger.info(msg(album, `${page.pagination.page}/${page.pagination.pages} page with ${page.results.length} release results found`));
       actions.releaseResults(album.id, page);
       const results = order(page.results, album);
       results.forEach((result, i) => {
-        db.getRelease(result.id).then(release => {
+        db.getRelease(result.id).then((release) => {
           logger.info(msg(album, `P(${page.pagination.page}/${page.pagination.pages}) R(${(i + 1)}/${results.length}) Release ${release.id} (master ${release.master_id}) retrieved`));
           actions.addRelease(release);
           if (i === page.results.length - 1 && page.pagination.page < page.pagination.pages) {
@@ -48,5 +45,5 @@ module.exports = function (db) {
     };
 
     db.search(params).then(loadAllReleases).catch(doCatch);
-  }
+  };
 };
