801c3cbb61bdb91ff35824594237336450d5e124
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-07 00:02:10 -0400

    Lint

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('static-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var albums = require('./routes/album');
+const routes = require('./routes/index');
+const users = require('./routes/users');
+const albums = require('./routes/album');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,34 +31,34 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/data/album', albums);
 
-/// catch 404 and forwarding to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+// / catch 404 and forwarding to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-/// error handlers
+// / error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
+  app.use((err, req, res) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use((err, req, res) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
   });
 });
 
