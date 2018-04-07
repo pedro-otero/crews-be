f933247bb9ce2266c9b244d3bccd2f7820f91554
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-06 23:48:57 -0400

    Lint

--- src/redux/view/build.spec.js
+++ src/redux/view/build.spec.js
@@ -2,52 +2,51 @@ const build = require('./build');
 const expect = require('../../../node_modules/expect.js/expect');
 const assert = require('assert');
 
-describe('Build', function () {
-  describe('creates a bundle from album and release information', function () {
-
+describe('Build', () => {
+  describe('creates a bundle from album and release information', () => {
     before(function () {
       this.album = {
         tracks: {
           items: [{
             name: 'Some track',
-          }]
-        }
+          }],
+        },
       };
       this.release = {
         extraartists: [{
           name: 'P2',
           role: 'Produced By',
-          tracks: '1'
+          tracks: '1',
         }, {
           name: 'C2',
           role: 'Lyrics By',
-          tracks: '1'
+          tracks: '1',
         }, {
           name: 'F2',
           role: 'Featuring',
-          tracks: '1'
+          tracks: '1',
         }, {
           name: 'A2',
           role: 'other instrument',
-          tracks: '1'
+          tracks: '1',
         }],
         tracklist: [{
           title: 'Some track',
           position: '1',
           extraartists: [{
             name: 'P1',
-            role: 'Producer'
+            role: 'Producer',
           }, {
             name: 'C1',
-            role: 'Written-By'
+            role: 'Written-By',
           }, {
             name: 'F1',
-            role: 'feat.'
+            role: 'feat.',
           }, {
             name: 'A1',
-            role: 'Some instrument'
-          }]
-        }]
+            role: 'Some instrument',
+          }],
+        }],
       };
       this.built = build(this.album, this.release);
     });
@@ -56,8 +55,7 @@ describe('Build', function () {
       expect(this.built.title).to.equal(this.album.name);
     });
 
-    describe('builds tracks', function () {
-
+    describe('builds tracks', () => {
       it('sets title', function () {
         expect(this.built.tracks[0].title).to.equal(this.album.tracks.items[0].name);
       });
@@ -88,18 +86,17 @@ describe('Build', function () {
 
       it('adds credits that are in the release track', function () {
         expect(this.built.tracks[0].credits).to.have.property('A1');
-        expect(this.built.tracks[0].credits['A1']).to.contain('Some instrument');
+        expect(this.built.tracks[0].credits.A1).to.contain('Some instrument');
       });
 
       it('adds credits that are in the release', function () {
         expect(this.built.tracks[0].credits).to.have.property('A2');
-        expect(this.built.tracks[0].credits['A2']).to.contain('other instrument');
+        expect(this.built.tracks[0].credits.A2).to.contain('other instrument');
       });
-
     });
   });
 
-  describe('works with x to y track string in release extra artists', function () {
+  describe('works with x to y track string in release extra artists', () => {
     beforeEach(function () {
       this.album = {
         tracks: {
@@ -109,28 +106,28 @@ describe('Build', function () {
             name: 'Track 2',
           }, {
             name: 'Track 3',
-          },]
-        }
+          }],
+        },
       };
       this.release = {
         extraartists: [{
           name: 'P1',
           role: 'Produced By',
-          tracks: '1 to 3'
+          tracks: '1 to 3',
         }],
         tracklist: [{
           title: 'Track 1',
           position: '1',
-          extraartists: []
+          extraartists: [],
         }, {
           title: 'Track 2',
           position: '2',
-          extraartists: []
+          extraartists: [],
         }, {
           title: 'Track 3',
           position: '3',
-          extraartists: []
-        },]
+          extraartists: [],
+        }],
       };
       this.built = build(this.album, this.release);
     });
