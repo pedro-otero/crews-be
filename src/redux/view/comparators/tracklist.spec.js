7c42a424a8e2c526c489f692468baa279926e0e9
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-06 23:54:52 -0400

    Lint

--- src/redux/view/comparators/tracklist.spec.js
+++ src/redux/view/comparators/tracklist.spec.js
@@ -1,34 +1,29 @@
 const assert = require('assert');
 const match = require('./tracklist');
 
-describe('Tracklist comparator', function () {
-
-  it('Different lengths', function () {
+describe('Tracklist comparator', () => {
+  it('Different lengths', () => {
     assert.equal(match([
-        { name: 'TrAck #1' },
-        { name: 'TrAck #1' },
-      ], [
-        { title: 'Track #1' },
-      ]
-    ), 0);
+      { name: 'TrAck #1' },
+      { name: 'TrAck #1' },
+    ], [
+      { title: 'Track #1' },
+    ]), 0);
   });
 
-  it('Exactly equal', function () {
+  it('Exactly equal', () => {
     assert.equal(match([
-        { name: 'TrAck #1' },
-      ], [
-        { title: 'Track #1' },
-      ]
-    ), 1);
+      { name: 'TrAck #1' },
+    ], [
+      { title: 'Track #1' },
+    ]), 1);
   });
 
-  it('Not that equal', function () {
+  it('Not that equal', () => {
     assert(match([
-        { name: '12345' },
-      ], [
-        { title: '12344' },
-      ]
-    ) < 1);
+      { name: '12345' },
+    ], [
+      { title: '12344' },
+    ]) < 1);
   });
-
 });
