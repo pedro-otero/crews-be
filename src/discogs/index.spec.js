41d209956a9f401ad5c7f665b47a1a54621f15ec
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-10 11:57:28 -0400

    Test that search results are added to the store

--- src/discogs/index.spec.js
+++ src/discogs/index.spec.js
@@ -4,35 +4,40 @@ const assert = require('assert');
 const Discogs = require('./index');
 const { actions } = require('../redux/state/index');
 
+const firstResults = {
+  pagination: {
+    pages: 2,
+    page: 1,
+  },
+  results: [{
+    id: 1,
+    title: 'Artist - Album',
+    year: '2000',
+  }],
+};
+
+const secondResults = {
+  pagination: {
+    pages: 2,
+    page: 2,
+  },
+  results: [{
+    id: 2,
+    title: 'Artist - Album',
+    year: '2000',
+  }],
+};
+
 describe('Find releases function', () => {
   describe('calls the db and actions functions', () => {
     before(function () {
       actions.addRelease = sinon.spy();
+      actions.releaseResults = sinon.spy();
       this.db = {
         search: sinon.stub()
-          .onCall(0).resolves({
-            pagination: {
-              pages: 2,
-              page: 1,
-            },
-            results: [{
-              id: 1,
-              title: 'Artist - Album',
-              year: '2000',
-            }],
-          })
+          .onCall(0).resolves(firstResults)
           .onCall(1)
-          .resolves({
-            pagination: {
-              pages: 2,
-              page: 2,
-            },
-            results: [{
-              id: 2,
-              title: 'Artist - Album',
-              year: '2000',
-            }],
-          }),
+          .resolves(secondResults),
         getRelease: sinon.stub()
           .onCall(0).resolves({ id: 1 })
           .onCall(1)
@@ -59,6 +64,10 @@ describe('Find releases function', () => {
       });
     });
 
+    it('adds 1st page of results to store', () => {
+      assert.deepEqual(actions.releaseResults.getCalls()[0].args[1], firstResults);
+    });
+
     it('search 2nd page', function () {
       assert.deepEqual(this.db.search.getCalls()[1].args[0], {
         artist: 'Artist',
@@ -67,6 +76,10 @@ describe('Find releases function', () => {
         per_page: 100,
         page: 2,
       });
+    });
+
+    it('adds 2nd page of results to store', () => {
+      assert.deepEqual(actions.releaseResults.getCalls()[0].args[1], secondResults);
     });
 
     it('getRelease 1', function () {
@@ -81,7 +94,7 @@ describe('Find releases function', () => {
       assert(this.db.getRelease.calledWith(2));
     });
 
-    it('addRelease 1', () => {
+    it('addRelease 2', () => {
       assert.deepEqual(actions.addRelease.getCalls(0)[1].args[0], { id: 2 });
     });
   });
