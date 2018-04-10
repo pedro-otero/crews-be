134c32db489c53c64986fccb6d3299e67ec1f1cf
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-10 11:01:16 -0400

    Test getRelease called

--- src/discogs/index.spec.js
+++ src/discogs/index.spec.js
@@ -4,10 +4,21 @@ const assert = require('assert');
 const Discogs = require('./index');
 
 describe('Find releases function', () => {
-  describe('calls the db functions', function () {
-    beforeEach(() => {
+  describe('calls the db functions', () => {
+    before(function () {
       this.db = {
-        search: sinon.spy(),
+        search: sinon.stub().resolves({
+          pagination: {
+            pages: 1,
+            page: 1,
+          },
+          results: [{
+            id: 1,
+            title: 'Artist - Album',
+            year: '2000',
+          }],
+        }),
+        getRelease: sinon.stub().resolves({}),
       };
       this.discogs = new Discogs(this.db);
       this.discogs.findReleases({
@@ -15,10 +26,12 @@ describe('Find releases function', () => {
         artists: [{
           name: 'Artist',
         }],
+        release_date: '2000',
+        album_type: 'album',
       });
     });
 
-    it('search', () => {
+    it('search', function () {
       assert(this.db.search.calledWith({
         release_title: 'Album',
         artist: 'Artist',
@@ -26,6 +39,10 @@ describe('Find releases function', () => {
         page: 1,
         per_page: 100,
       }));
+    });
+
+    it('getRelease', function () {
+      assert(this.db.getRelease.calledWith(1));
     });
   });
 });
