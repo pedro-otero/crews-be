8e6f98faf39d2fb34e2ff5fcfd4ae538e50c60ea
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-13 20:37:18 -0400

    Refactor test

--- src/search/index.spec.js
+++ src/search/index.spec.js
@@ -3,10 +3,6 @@ const assert = require('assert');
 
 const search = require('./index');
 
-const spotify = Promise.resolve({
-  getAlbum: sinon.stub().resolves({})
-});
-
 const discogs = {
   findReleases: () => Rx.Observable.create(observer => {
     observer.next({});
@@ -15,13 +11,22 @@ const discogs = {
   })
 };
 
-const store ={
-  getState:()=>({searches:[]})
+const store = {
+  getState: () => ({ searches: [] })
 }
 
 describe('Search function', function () {
-  it('Returns a newly created search', function () {
-    const result = search(spotify, discogs, store)(1);
-    assert.equal(1, result.id);
+  describe('Spotify getAlbum exists', function () {
+    beforeEach(function () {
+      const spotify = Promise.resolve({
+        getAlbum: sinon.stub().resolves({})
+      });
+      this.search = search(spotify, discogs, store);
+    });
+
+    it('Returns a newly created search', function () {
+      const result = this.search(1);
+      assert.equal(1, result.id);
+    });
   });
 });
