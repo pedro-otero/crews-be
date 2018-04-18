5411bc5f02d743f13bff316102d2ff04e88f6a3a
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-17 22:48:03 -0400

    Use sort

--- src/search/order.js
+++ src/search/order.js
@@ -1,30 +1,19 @@
-module.exports = (results, {
-  name,
-  artists: [{ name: artist }],
-  release_date: releaseDate,
-  album_type: albumType,
-}) => results.map((data) => {
-  const {
-    title,
-    year,
-    format,
-  } = data;
-  const comparisons = [
-    `${artist} - ${name}` === title,
-    title.match(`.+ - ${name.replace(/(.+) \((.+)\)/, '$1').toUpperCase()}`),
-    year === releaseDate.substring(0, 4),
+module.exports = (results, album) => results.sort((a, b) => {
+  const score = ({ title, year, format }) => [
+
+    // Exact title
+    `${album.artists[0].name} - ${album.name}` === title,
+
+    // Album title part of the full release title
+    title.match(`.+ - ${album.name.replace(/(.+) \((.+)\)/, '$1').toUpperCase()}`),
+
+    // Year
+    year === album.release_date.substring(0, 4),
+
+    // Format
     (((typeof format === 'string') ?
       format.split(', ') :
-      (format || [])).map(f => f.toUpperCase())).includes(albumType.toUpperCase()),
-  ];
-  const score = comparisons.reduce((accum, result) => accum + Number(result), 0);
-  return { data, score };
-}).reduce((ordered, item) => {
-  const position = ordered.findIndex(innerItem => innerItem.score < item.score);
-  if (position === -1) {
-    return ordered.concat([item]);
-  }
-  const modified = [...ordered];
-  modified.splice(position, 0, item);
-  return modified;
-}, []).map(item => item.data);
+      (format || [])).map(f => f.toUpperCase())).includes(album.album_type.toUpperCase()),
+  ].reduce((accum, result) => accum + Number(result));
+  return score(b) - score(a);
+});
