d95f2b1abe69cb8f7a254a22480f225ba51f95d2
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-18 10:17:05 -0400

    lint

--- src/redux/state/reducer/searches.js
+++ src/redux/state/reducer/searches.js
@@ -7,12 +7,14 @@ module.exports = (state = [], {
   type, id, errors,
 }) => {
   switch (type) {
-    case ADD_SEARCH:
+    case ADD_SEARCH: {
       return state.concat({ id, errors: [] });
-    case PUT_ERRORS:
+    }
+    case PUT_ERRORS: {
       const search = state.find(s => s.id === id);
       Object.assign(search, { errors: search.errors.concat(errors) });
       return state.filter(s => s.id !== id).concat(search);
+    }
     default:
       return state;
   }
