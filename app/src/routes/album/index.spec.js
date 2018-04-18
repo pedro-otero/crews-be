2dc8ac304cd1188448cdf900644b2ce2b8b2e22d
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-18 17:52:08 -0400

    Refactor

--- app/src/routes/album/index.spec.js
+++ app/src/routes/album/index.spec.js
@@ -1,21 +1,16 @@
 const express = require('express');
-const request = require('supertest');
+const Request = require('supertest');
 
 const route = require('./index');
 
 describe('Albums endpoint', () => {
-  beforeEach(function () {
+  it('returns 200', (done) => {
     const app = express();
     app.locals.searchAlbum = () => ({
       start: () => Promise.resolve({ status: 200 }),
     });
     app.use('/data/album', route);
-    this.request = request(app);
-  });
-
-  it('returns 200', function (done) {
-    this.request
-      .get('/data/album/1')
-      .expect(200, done);
+    const request = Request(app);
+    request.get('/data/album/1').expect(200, done);
   });
 });
