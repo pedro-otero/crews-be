e4392e75f523f20a61c6f576f1291837f35a788f
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-17 23:30:17 -0400

    Replace order dependency

--- src/search/discogs.js
+++ src/search/discogs.js
@@ -1,6 +1,24 @@
 const Rx = require('rxjs');
 
-const order = require('./order');
+const order = (results, album) => results.sort((a, b) => {
+  const score = ({ title, year, format }) => [
+
+    // Exact title
+    `${album.artists[0].name} - ${album.name}` === title,
+
+    // Album title part of the full release title
+    title.match(`.+ - ${album.name.replace(/(.+) \((.+)\)/, '$1').toUpperCase()}`),
+
+    // Year
+    year === album.release_date.substring(0, 4),
+
+    // Format
+    (((typeof format === 'string') ?
+      format.split(', ') :
+      (format || [])).map(f => f.toUpperCase())).includes(album.album_type.toUpperCase()),
+  ].reduce((accum, result) => accum + Number(result));
+  return score(b) - score(a);
+});
 
 module.exports = function (db) {
   const search = (album, page) => db.search({
