4a641f53444dcf114c1567c5a400881a7051474a
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-18 20:39:25 -0400

    Test failed login

--- app/src/setup/spotify.spec.js
+++ app/src/setup/spotify.spec.js
@@ -16,5 +16,14 @@ describe('Spotify module', () => {
       });
       spotify.getApi().then(assert).then(done);
     });
+
+    it('rejects with error', (done) => {
+      const spotify = Spotify(function () {
+        this.clientCredentialsGrant = () => Promise.resolve({
+          statusCode: 400,
+        });
+      });
+      spotify.getApi().then(() => assert(false), assert).then(done);
+    });
   });
 });
