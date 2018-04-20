92893ec52f4db8863ed5b70daeef578432614728
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-19 20:39:08 -0400

    Test picking best match

--- app/src/redux/view/query.spec.js
+++ app/src/redux/view/query.spec.js
@@ -186,5 +186,33 @@ describe('Search state view', () => {
       const query = Query(1, store);
       assert.equal(query.progress, 100);
     });
+
+    it('picks the best match', function () {
+      const store = this.mockStore({
+        albums: [{
+          id: 1,
+          tracks: { items: [{ name: 'track' }] },
+        }],
+        results: [{
+          album: 1,
+          page: {
+            pagination: {
+              page: 1,
+              pages: 1,
+              items: 2,
+            },
+            results: [{ id: 1 }, { id: 2 }],
+          },
+        }],
+        releases: [
+          {
+            id: 1,
+            tracklist: [{ title: 'similar track' }],
+          },
+          { id: 2, tracklist: [{ title: 'track', extraartists: [{ name: 'some guy', role: 'Producer' }] }] }],
+      });
+      const query = Query(1, store);
+      assert.equal(query.bestMatch.tracks[0].producers[0], 'some guy');
+    });
   });
 });
