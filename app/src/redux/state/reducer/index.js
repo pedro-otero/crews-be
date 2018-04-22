a0ef0254a615cec05114cf5ba5885c983d3d0435
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-22 11:32:30 -0400

    Add credits reducer to index

--- app/src/redux/state/reducer/index.js
+++ app/src/redux/state/reducer/index.js
@@ -2,7 +2,8 @@ const searches = require('./searches');
 const albums = require('./albums');
 const releases = require('./releases');
 const results = require('./results');
+const credits = require('./results');
 
 module.exports = ({
-  searches, albums, releases, results,
+  searches, albums, releases, results, credits,
 });
