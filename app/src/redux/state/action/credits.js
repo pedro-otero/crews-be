39b378f9ee539d79165b425ea3baa227ff09b055
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-22 11:00:14 -0400

    Refactor

--- app/src/redux/state/action/credits.js
+++ app/src/redux/state/action/credits.js
@@ -11,34 +11,33 @@ module.exports = ({ tracks: { items } }, { tracklist, extraartists: releaseExtra
     const right = translatePosition(extremes[1]);
     return (left <= p) && (p <= right);
   };
-  const temp = tracklist.map(({ position, extraartists = [] }) => ({
-    position,
-    extraartists: extraartists.concat(releaseExtraArtists
-      .filter(({ tracks, role }) => !!tracks && !!role)
-      .filter(({ tracks }) => splitTrim(tracks, ',')
-        .reduce((accum, trackString) => accum || (() => {
-          if (trackString.includes('-')) {
-            return inRange(trackString, '-', position);
-          } else if (trackString.includes('to')) {
-            return inRange(trackString, 'to', position);
-          }
-          return trackString.split(',').map(t => t.trim()).includes(position);
-        })(), false))
-      .reduce((accum, { role, name }) => accum.concat([{ role, name }]), [])),
-  })).map(({ extraartists: credits }, i) => ({
-    id: items[i].id,
-    credits: credits.reduce((trackCredits, { name, role }) => trackCredits
-      .concat(splitTrim(role, ',').map(r => ({
-        name,
-        role: r,
-      }))), []),
-  })).reduce(
-    (accum, { id, credits }) =>
-      accum.concat(credits
-        .map(({ name, role }) => ({ track: id, name, role }))),
-    []);
   return {
     type: ADD_CREDITS,
-    credits: temp,
+    credits: tracklist.map(({ position, extraartists = [] }) => ({
+      position,
+      extraartists: extraartists.concat(releaseExtraArtists
+        .filter(({ tracks, role }) => !!tracks && !!role)
+        .filter(({ tracks }) => splitTrim(tracks, ',')
+          .reduce((accum, trackString) => accum || (() => {
+            if (trackString.includes('-')) {
+              return inRange(trackString, '-', position);
+            } else if (trackString.includes('to')) {
+              return inRange(trackString, 'to', position);
+            }
+            return trackString.split(',').map(t => t.trim()).includes(position);
+          })(), false))
+        .reduce((accum, { role, name }) => accum.concat([{ role, name }]), [])),
+    })).map(({ extraartists: credits }, i) => ({
+      id: items[i].id,
+      credits: credits.reduce((trackCredits, { name, role }) => trackCredits
+        .concat(splitTrim(role, ',').map(r => ({
+          name,
+          role: r,
+        }))), []),
+    })).reduce(
+      (accum, { id, credits }) =>
+        accum.concat(credits
+          .map(({ name, role }) => ({ track: id, name, role }))),
+      []),
   };
 };
