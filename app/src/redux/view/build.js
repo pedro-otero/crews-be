f96a4fccd4f2b46991d0872179b5ba0b1b21b65d
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-22 16:08:34 -0400

    Test add credits
    (cherry picked from commit ff21415)

--- app/src/redux/view/build.js
+++ app/src/redux/view/build.js
@@ -1,54 +1,18 @@
 const roles = require('./roles');
 
-function splitRange(tracksstring) {
-  const split = tracksstring.split('to')
-    .map(s => s.trim());
-  const start = Number(split[0]);
-  const end = Number(split[1]);
-  return [...Array((end - start) + 1).keys()].map((item, i) => String(start + i));
-}
-
-const getTracksList = tracksstring => tracksstring
-  .split(',')
-  .map(s => s.trim())
-  .map(el => (el.includes('to') ? splitRange(el) : el))
-  .reduce((a, b) => a.concat(b), []);
-
-const reduce = allCredits => highlightedRole => allCredits
-  .filter(credit => credit.role.split(', ')
-    .some(role => roles[highlightedRole].includes(role)))
-  .map(credit => credit.name);
-
-const buildSong = ({ tracklist, extraartists = [] }) => (spotifyTrack, trackIndex) => {
-  const track = tracklist[trackIndex];
-  const position = track.position || String(trackIndex + 1);
-  const allCredits = extraartists
-    .filter(credit => getTracksList(credit.tracks)
-      .includes(position))
-    .concat(track.extraartists || []);
-  return {
-    title: spotifyTrack.name,
-    producers: reduce(allCredits)('producers'),
-    composers: reduce(allCredits)('composers'),
-    featured: reduce(allCredits)('featured'),
-    credits: allCredits.reduce((credits, credit) => {
-      const newCredits = credit.role.split(', ')
-        .filter(role => !roles.producers.includes(role) &&
-          !roles.composers.includes(role) &&
-          !roles.featured.includes(role));
-      const result = Object.assign({}, credits);
-      if (newCredits.length) {
-        if (!(result.name in result)) {
-          result[credit.name] = [];
-        }
-        result[credit.name].push(...newCredits);
-      }
-      return result;
-    }, {}),
-  };
-};
-
-module.exports = (spotifyAlbum, discogsRelease) => ({
-  tracks: spotifyAlbum.tracks.items.length ?
-    spotifyAlbum.tracks.items.map(buildSong(discogsRelease)) : [],
+module.exports = (spotifyAlbum, credits) => ({
+  tracks: spotifyAlbum.tracks.items.map(track => ({
+    id: track.id,
+    title: track.name,
+    trackCredits: credits.filter(credit => credit.track === track.id),
+  })).map(({ id, title, trackCredits }) => ({
+    id,
+    title,
+    producers: [],
+    composers: [],
+    featured: [],
+    credits: trackCredits.reduce((tree, credit) => Object.assign({}, tree, ({
+      [credit.name]: (tree[credit.name] || []).concat([credit.role]),
+    })), {}),
+  })),
 });
--- app/src/redux/view/build.spec.js
+++ app/src/redux/view/build.spec.js
@@ -2,149 +2,81 @@ const build = require('./build');
 const assert = require('assert');
 
 describe('Build', () => {
-  describe('creates a bundle from album and release information', () => {
+  describe('creates an empty bundle with an empty album', () => {
     before(function () {
-      this.album = {
-        tracks: {
-          items: [{
-            name: 'Some track',
-          }],
-        },
-      };
-      this.release = {
-        extraartists: [{
-          name: 'P2',
-          role: 'Produced By',
-          tracks: '1',
-        }, {
-          name: 'C2',
-          role: 'Lyrics By',
-          tracks: '1',
-        }, {
-          name: 'F2',
-          role: 'Featuring',
-          tracks: '1',
-        }, {
-          name: 'A2',
-          role: 'other instrument',
-          tracks: '1',
-        }],
-        tracklist: [{
-          title: 'Some track',
-          position: '1',
-          extraartists: [{
-            name: 'P1',
-            role: 'Producer',
-          }, {
-            name: 'C1',
-            role: 'Written-By',
-          }, {
-            name: 'F1',
-            role: 'feat.',
-          }, {
-            name: 'A1',
-            role: 'Some instrument',
-          }],
-        }],
-      };
-      this.built = build(this.album, this.release);
+      const credits = [{}];
+      const album = { tracks: { items: [] } };
+      this.bundle = build(album, credits);
     });
 
-    it('sets title', function () {
-      assert.equal(this.album.name, this.built.title);
-    });
-
-    describe('builds tracks', () => {
-      it('sets title', function () {
-        assert.equal(this.album.tracks.items[0].name, this.built.tracks[0].title);
-      });
-
-      it('adds producers that are in the release track', function () {
-        assert(this.built.tracks[0].producers.includes('P1'));
-      });
-
-      it('adds producers that are in the release', function () {
-        assert(this.built.tracks[0].producers.includes('P2'));
-      });
-
-      it('adds composers that are in the release track', function () {
-        assert(this.built.tracks[0].composers.includes('C1'));
-      });
-
-      it('adds composers that are in the release', function () {
-        assert(this.built.tracks[0].composers.includes('C2'));
-      });
-
-      it('adds featured artists that are in the release track', function () {
-        assert(this.built.tracks[0].featured.includes('F1'));
-      });
-
-      it('adds featured artists that are in the release', function () {
-        assert(this.built.tracks[0].featured.includes('F2'));
-      });
-
-      it('adds credits that are in the release track', function () {
-        assert(Object.keys(this.built.tracks[0].credits).includes('A1'));
-        assert(this.built.tracks[0].credits.A1.includes('Some instrument'));
-      });
-
-      it('adds credits that are in the release', function () {
-        assert(Object.keys(this.built.tracks[0].credits).includes('A2'));
-        assert(this.built.tracks[0].credits.A2.includes('other instrument'));
-      });
+    it('has 0 tracks', function () {
+      assert.equal(this.bundle.tracks.length, 0);
     });
   });
 
-  describe('works with x to y track string in release extra artists', () => {
-    beforeEach(function () {
-      this.album = {
+  describe('creates an empty bundle with an list of credits', () => {
+    before(function () {
+      const credits = [{}];
+      const album = {
         tracks: {
           items: [{
+            id: 'T1',
             name: 'Track 1',
-          }, {
-            name: 'Track 2',
-          }, {
-            name: 'Track 3',
           }],
         },
       };
-      this.release = {
-        extraartists: [{
-          name: 'P1',
-          role: 'Produced By',
-          tracks: '1 to 3',
-        }],
-        tracklist: [{
-          title: 'Track 1',
-          position: '1',
-          extraartists: [],
-        }, {
-          title: 'Track 2',
-          position: '2',
-          extraartists: [],
-        }, {
-          title: 'Track 3',
-          position: '3',
-          extraartists: [],
-        }],
+      this.bundle = build(album, credits);
+    });
+
+    it('has 1 track', function () {
+      assert.equal(this.bundle.tracks.length, 1);
+    });
+
+    it('track has title', function () {
+      assert.equal(this.bundle.tracks[0].title, 'Track 1');
+    });
+
+    it('track has id', function () {
+      assert.equal(this.bundle.tracks[0].id, 'T1');
+    });
+
+    it('track has no producers', function () {
+      assert.equal(this.bundle.tracks[0].producers.length, 0);
+    });
+
+    it('track has no composers', function () {
+      assert.equal(this.bundle.tracks[0].composers.length, 0);
+    });
+
+    it('track has no featured artists', function () {
+      assert.equal(this.bundle.tracks[0].featured.length, 0);
+    });
+
+    it('track has no credits', function () {
+      assert.equal(Object.keys(this.bundle.tracks[0].credits).length, 0);
+    });
+  });
+
+  describe('creates a bundle from a list of credits', () => {
+    before(function () {
+      const credits = [{
+        name: 'P1',
+        role: 'R1',
+        track: 'T1',
+      }];
+      const album = {
+        tracks: {
+          items: [{
+            id: 'T1',
+            name: 'Track 1',
+          }],
+        },
       };
-      this.built = build(this.album, this.release);
+      this.bundle = build(album, credits);
     });
 
-    it('built bundle has 3 tracks', function () {
-      assert.equal(3, this.built.tracks.length);
-    });
-
-    it('built bundle has P1 as producer of Track 1', function () {
-      assert.equal('P1', this.built.tracks[0].producers[0]);
-    });
-
-    it('built bundle has P1 as producer of Track 2', function () {
-      assert.equal('P1', this.built.tracks[1].producers[0]);
-    });
-
-    it('built bundle has P1 as producer of Track 3', function () {
-      assert.equal('P1', this.built.tracks[2].producers[0]);
+    it('has P1 as R1 of T1', function () {
+      assert.equal(this.bundle.tracks[0].credits.P1[0], 'R1');
     });
   });
 });
