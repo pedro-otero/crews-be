a0aa0c1c5c845f9e10f8f5122633ae9d86cdfa22
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-21 22:39:13 -0400

    Creates simple credit

--- app/src/redux/state/action/credits.js
+++ app/src/redux/state/action/credits.js
@@ -1,7 +1,8 @@
 const { ADD_CREDITS } = require('./constants');
 
-module.exports = (album, release) => {
-  const credits = [];
+module.exports = ({ tracks: { items } }, { tracklist }) => {
+  const credits = items.reduce((all, { id: track }, i) => all
+    .concat(tracklist[i].extraartists.map(({ name, role }) => ({ track, name, role }))), []);
   return {
     type: ADD_CREDITS,
     credits,
--- app/src/redux/state/action/credits.spec.js
+++ app/src/redux/state/action/credits.spec.js
@@ -6,16 +6,34 @@ const {
 const create = require('./credits');
 
 describe('Credits action creator', () => {
-  const album = {};
+  const album = {
+    tracks: {
+      items: [{ id: 'T1' }],
+    },
+  };
 
   describe('adds credits', () => {
     before(function () {
-      const release = {};
+      const release = {
+        tracklist: [{
+          extraartists: [{
+            name: 'P1',
+            role: 'R1',
+          }],
+        }],
+      };
       this.action = create(album, release);
     });
 
     it('type', function () {
       assert.equal(this.action.type, ADD_CREDITS);
+    });
+
+    it('extract single role track credit', function () {
+      assert(!!this.action.credits.find(credit =>
+        credit.track === 'T1' &&
+        credit.name === 'P1' &&
+        credit.role === 'R1'));
     });
   });
 });
