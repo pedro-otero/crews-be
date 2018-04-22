3b9c782b880da8d79452dbbef8a83b2adf45a61f
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-22 10:34:05 -0400

    Lint

--- app/src/redux/state/action/credits.js
+++ app/src/redux/state/action/credits.js
@@ -1,23 +1,26 @@
 const { ADD_CREDITS } = require('./constants');
 
-const splitTrim = (value,separator) => value.split(separator).map(v => v.trim());
+const splitTrim = (value, separator) => value.split(separator).map(v => v.trim());
 
 module.exports = ({ tracks: { items } }, { tracklist, extraartists: releaseExtraArtists }) => {
   const translatePosition = position => tracklist.findIndex(t => t.position === position);
-  const inRange=(trackString,separator,position)=> {
-    const extremes = splitTrim(trackString,separator);
-    return (translatePosition(extremes[0]) <= translatePosition(position)) && (translatePosition(position) <= translatePosition(extremes[1]));
+  const inRange = (trackString, separator, position) => {
+    const extremes = splitTrim(trackString, separator);
+    const left = translatePosition(extremes[0]);
+    const p = translatePosition(position);
+    const right = translatePosition(extremes[1]);
+    return (left <= p) && (p <= right);
   };
   const temp = tracklist.map(({ position, extraartists = [] }) => ({
     position,
     extraartists: extraartists.concat(releaseExtraArtists
       .filter(({ tracks, role }) => !!tracks && !!role)
-      .filter(({ tracks }) => splitTrim(tracks,',')
+      .filter(({ tracks }) => splitTrim(tracks, ',')
         .reduce((accum, trackString) => accum || (() => {
           if (trackString.includes('-')) {
-            return inRange(trackString,'-',position);
+            return inRange(trackString, '-', position);
           } else if (trackString.includes('to')) {
-            return inRange(trackString,'to',position);
+            return inRange(trackString, 'to', position);
           }
           return trackString.split(',').map(t => t.trim()).includes(position);
         })(), false))
@@ -25,7 +28,7 @@ module.exports = ({ tracks: { items } }, { tracklist, extraartists: releaseExtra
   })).map(({ extraartists: credits }, i) => ({
     id: items[i].id,
     credits: credits.reduce((trackCredits, { name, role }) => trackCredits
-      .concat(splitTrim(role,',').map(r => ({
+      .concat(splitTrim(role, ',').map(r => ({
         name,
         role: r,
       }))), []),
