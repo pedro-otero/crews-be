9bf87dc68846d9638c8078a5318f87c2123672b7
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-23 15:15:46 -0400

    Use correct reducer

--- app/src/redux/state/reducer/index.js
+++ app/src/redux/state/reducer/index.js
@@ -2,7 +2,7 @@ const searches = require('./searches');
 const albums = require('./albums');
 const releases = require('./releases');
 const results = require('./results');
-const credits = require('./results');
+const credits = require('./credits');
 
 module.exports = ({
   searches, albums, releases, results, credits,
