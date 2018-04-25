83d2ada284240e7a971bd638350b87d6af0ef44c
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-25 11:34:58 -0400

    Fuse modules

--- app/src/search/index.js
+++ app/src/search/index.js
@@ -1,7 +1,54 @@
+const Rx = require('rxjs');
+
 const { actions } = require('../redux/state');
-const DiscogFinder = require('./discogs');
 
 const spotifyErrorMessages = require('./spotify-errors');
+
+function DiscogsFinder(db) {
+  const search = (album, page) => db.search({
+    artist: album.artists[0].name,
+    release_title: album.name.replace(/(.+) \((.+)\)/, '$1'),
+    type: 'release',
+    per_page: 100,
+    page,
+  });
+
+  const findReleases = album => Rx.Observable.create((observer) => {
+    const fetch = async (p) => {
+      try {
+        search(album, p).then(async (page) => {
+          observer.next({ type: 'results', data: { page } });
+          // eslint-disable-next-line no-restricted-syntax
+          for (const result of page.results) {
+            try {
+              // eslint-disable-next-line no-await-in-loop
+              const release = await db.getRelease(result.id);
+              observer.next({ type: 'release', data: { release } });
+            } catch (error) {
+              observer.error(error);
+              return;
+            }
+          }
+          if (page.pagination.page < page.pagination.pages) {
+            fetch(page.pagination.page + 1);
+          } else {
+            observer.complete();
+          }
+        }, (error) => {
+          observer.error(error);
+        }).catch((error) => {
+          observer.error(error);
+        });
+      } catch (error) {
+        observer.error(error);
+      }
+    };
+
+    fetch(1);
+  });
+
+  return { findReleases };
+}
 
 const observer = (logger, output) => ({
   next: ({ type, data: { page, release } }) => ({
@@ -55,7 +102,7 @@ module.exports = (spotify, db, createLogger) => (id) => {
     return Error(spotifyErrorMessages.general);
   };
 
-  const discogs = new DiscogFinder(db);
+  const discogs = new DiscogsFinder(db);
 
   const start = () => new Promise((resolve, reject) => {
     const output = actionsWrapper(id);
