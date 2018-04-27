2899550a00c46b32353471afbe4f156e1aaa1b94
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-27 11:23:06 -0400

    Refactor

--- app/src/search/logger.js
+++ app/src/search/logger.js
@@ -1,16 +1,13 @@
 const winston = require('winston');
 
-const levels = {
-  info: 0,
-  error: 1,
-};
-const createTransports = albumId => [
-  new winston.transports.Console({ level: 'error' }),
-  new winston.transports.File({ filename: `app/log/${albumId}.log`, level: 'error' }),
-];
-
-module.exports = album => winston.createLogger({
-  levels,
+module.exports = ({ id }) => winston.createLogger({
+  levels: {
+    info: 0,
+    error: 1,
+  },
   format: winston.format.simple(),
-  transports: createTransports(album.id),
+  transports: [
+    new winston.transports.Console({ level: 'error' }),
+    new winston.transports.File({ filename: `app/log/${id}.log`, level: 'error' }),
+  ],
 });
