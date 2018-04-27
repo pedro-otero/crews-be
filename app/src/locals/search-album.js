32502d62144fb23c847c2f14acd1010c3f66700d
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-27 11:25:35 -0400

    Move logger setup into search-album locals module

--- app/src/locals/search-album.js
+++ app/src/locals/search-album.js
@@ -1,8 +1,20 @@
 const SpotifyWebApi = require('spotify-web-api-node');
+const winston = require('winston');
 
 const spotify = require('../api/spotify');
 const discogs = require('../api/discogs');
 const search = require('../search');
-const createLogger = require('../search/logger');
+
+const createLogger = ({ id }) => winston.createLogger({
+  levels: {
+    info: 0,
+    error: 1,
+  },
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console({ level: 'error' }),
+    new winston.transports.File({ filename: `app/log/${id}.log`, level: 'error' }),
+  ],
+});
 
 module.exports = search(spotify(SpotifyWebApi), discogs, createLogger);
