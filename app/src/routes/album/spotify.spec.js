f08c0809b1748c195cdcec4eff9d3034223404f1
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-29 14:21:42 -0400

    Setup

--- app/src/routes/album/spotify.spec.js
+++ app/src/routes/album/spotify.spec.js
@@ -5,7 +5,7 @@ const route = require('./spotify');
 
 describe('Spotify middleware', () => {
   describe('Nothing fails', () => {
-    it('calls next middleware after getting album', function (done) {
+    before(function () {
       const app = express();
       app.use('/data/album', route);
       app.use('/data/album', (req, res) => {
@@ -17,6 +17,9 @@ describe('Spotify middleware', () => {
         }),
       };
       this.request = Request(app);
+    });
+
+    it('calls next middleware after getting album', function (done) {
       this.request.get('/data/album/1').expect('NEXT CALLED', done);
     });
   });
