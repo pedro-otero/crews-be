12be2fdff825161892fb6b6ed6aa4fa9f251f916
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-29 15:07:47 -0400

    Just call next middleware if album is in store already

--- app/src/routes/album/spotify.js
+++ app/src/routes/album/spotify.js
@@ -5,13 +5,18 @@ const router = express.Router();
 module.exports = router.get('/:spotifyAlbumId', (req, res, next) => {
   const {
     app: {
-      locals: { spotify, actions },
+      locals: { spotify, actions, store },
     },
     params: { spotifyAlbumId },
   } = req;
 
-  spotify.getApi().then(api => api.getAlbum(spotifyAlbumId)).then(({ body }) => {
-    actions.addAlbum(body);
+  const album = store.getState().albums.find(a => a.id === spotifyAlbumId);
+  if (album) {
     next();
-  });
+  } else {
+    spotify.getApi().then(api => api.getAlbum(spotifyAlbumId)).then(({ body }) => {
+      actions.addAlbum(body);
+      next();
+    });
+  }
 });
--- app/src/routes/album/spotify.spec.js
+++ app/src/routes/album/spotify.spec.js
@@ -6,7 +6,7 @@ const assert = require('assert');
 const route = require('./spotify');
 
 describe('Spotify middleware', () => {
-  describe('Album is not in store', () => {
+  describe('Album is NOT in store', () => {
     before(function () {
       const app = express();
       app.use('/data/album', route);
@@ -45,6 +45,50 @@ describe('Spotify middleware', () => {
     it('adds album to the store', function (done) {
       this.request.get('/data/album/1').expect('NEXT CALLED', () => {
         assert.equal(this.actions.addAlbum.getCalls()[0].args[0].id, 1);
+        done();
+      });
+    });
+  });
+
+  describe('Album is in store', () => {
+    beforeEach(function () {
+      const app = express();
+      app.use('/data/album', route);
+      app.use('/data/album', (req, res) => {
+        res.status(200).send('NEXT CALLED');
+      });
+      this.api = {
+        getAlbum: sinon.stub().resolves({ body: { id: 1 } }),
+      };
+      this.actions = {
+        addAlbum: sinon.stub(),
+      };
+      app.locals.spotify = {
+        getApi: () => Promise.resolve(this.api),
+      };
+      app.locals.actions = this.actions;
+      app.locals.store = {
+        getState: () => ({
+          albums: [{ id: '1' }],
+        }),
+      };
+      this.request = Request(app);
+    });
+
+    it('calls next middleware after getting album', function (done) {
+      this.request.get('/data/album/1').expect('NEXT CALLED', done);
+    });
+
+    it('does not get album again', function (done) {
+      this.request.get('/data/album/1').expect('NEXT CALLED', () => {
+        assert.equal(this.api.getAlbum.getCalls().length, 0);
+        done();
+      });
+    });
+
+    it('does not add album to the store', function (done) {
+      this.request.get('/data/album/1').expect('NEXT CALLED', () => {
+        assert.equal(this.actions.addAlbum.getCalls().length, 0);
         done();
       });
     });
