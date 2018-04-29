461f4470aacfca3fef23f759920d899741e445df
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-28 20:44:59 -0400

    Add detail level only two log file

--- app/src/locals/search-album.js
+++ app/src/locals/search-album.js
@@ -9,11 +9,12 @@ const createLogger = ({ id }) => winston.createLogger({
   levels: {
     say: 0,
     notice: 1,
+    detail: 2,
   },
   format: winston.format.simple(),
   transports: [
     new winston.transports.Console({ level: 'notice' }),
-    new winston.transports.File({ filename: `app/log/${id}.log`, level: 'notice' }),
+    new winston.transports.File({ filename: `app/log/${id}.log`, level: 'detail' }),
   ],
 });
 
