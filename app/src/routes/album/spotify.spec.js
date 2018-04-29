f84faf8249d52b1f8087004a514aa31d3e5d88d4
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-29 15:58:27 -0400

    Use common all common setup

--- app/src/routes/album/spotify.spec.js
+++ app/src/routes/album/spotify.spec.js
@@ -14,73 +14,50 @@ describe('Spotify middleware', () => {
     });
     app.locals.store = {
       getState: () => ({
-        albums: [{ id: '2' }],
+        albums: [{ id: 'A2' }],
       }),
     };
     this.actions = {
       addAlbum: sinon.stub(),
     };
     app.locals.actions = this.actions;
-    this.app = app;
+    const getAlbum = id => Promise.resolve({
+      A1: { body: { id: 'A1' } },
+    }[id]);
+    this.api = {
+      getAlbum: sinon.stub().callsFake(getAlbum),
+    };
+    app.locals.spotify = {
+      getApi: () => Promise.resolve(this.api),
+    };
+    this.request = Request(app);
   });
 
   describe('Album is NOT in store', () => {
-    beforeEach(function () {
-      this.api = {
-        getAlbum: sinon.stub().resolves({ body: { id: 1 } }),
-      };
-      this.app.locals.spotify = {
-        getApi: () => Promise.resolve(this.api),
-      };
-      this.request = Request(this.app);
+    beforeEach(function (done) {
+      this.request.get('/data/album/A1').expect('NEXT CALLED', done);
     });
 
-    it('calls next middleware after getting album', function (done) {
-      this.request.get('/data/album/1').expect('NEXT CALLED', done);
+    it('gets the album with the id passed in the path', function () {
+      assert.equal(this.api.getAlbum.getCalls()[0].args[0], 'A1');
     });
 
-    it('gets the album with the id passed in the path', function (done) {
-      this.request.get('/data/album/1').expect('NEXT CALLED', () => {
-        assert.equal(this.api.getAlbum.getCalls()[0].args[0], 1);
-        done();
-      });
-    });
-
-    it('adds album to the store', function (done) {
-      this.request.get('/data/album/1').expect('NEXT CALLED', () => {
-        assert.equal(this.actions.addAlbum.getCalls()[0].args[0].id, 1);
-        done();
-      });
+    it('adds album to the store', function () {
+      assert.equal(this.actions.addAlbum.getCalls()[0].args[0].id, 'A1');
     });
   });
 
   describe('Album is in store', () => {
-    beforeEach(function () {
-      this.api = {
-        getAlbum: sinon.stub().resolves({ body: { id: 1 } }),
-      };
-      this.app.locals.spotify = {
-        getApi: () => Promise.resolve(this.api),
-      };
-      this.request = Request(this.app);
+    beforeEach(function (done) {
+      this.request.get('/data/album/A2').expect('NEXT CALLED', done);
     });
 
-    it('calls next middleware after getting album', function (done) {
-      this.request.get('/data/album/2').expect('NEXT CALLED', done);
+    it('does not get album again', function () {
+      assert.equal(this.api.getAlbum.getCalls().length, 0);
     });
 
-    it('does not get album again', function (done) {
-      this.request.get('/data/album/2').expect('NEXT CALLED', () => {
-        assert.equal(this.api.getAlbum.getCalls().length, 0);
-        done();
-      });
-    });
-
-    it('does not add album to the store', function (done) {
-      this.request.get('/data/album/2').expect('NEXT CALLED', () => {
-        assert.equal(this.actions.addAlbum.getCalls().length, 0);
-        done();
-      });
+    it('does not add album to the store', function () {
+      assert.equal(this.actions.addAlbum.getCalls().length, 0);
     });
   });
 });
