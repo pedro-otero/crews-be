0af5f48e4ede84d1304084d78faf7f2f5ffec544
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-29 14:20:53 -0400

    Refactor

--- app/src/routes/album/spotify.spec.js
+++ app/src/routes/album/spotify.spec.js
@@ -4,18 +4,20 @@ const Request = require('supertest');
 const route = require('./spotify');
 
 describe('Spotify middleware', () => {
-  it('calls next middleware after getting album', (done) => {
-    const app = express();
-    app.use('/data/album', route);
-    app.use('/data/album', (req, res) => {
-      res.status(200).send('NEXT CALLED');
+  describe('Nothing fails', () => {
+    it('calls next middleware after getting album', function (done) {
+      const app = express();
+      app.use('/data/album', route);
+      app.use('/data/album', (req, res) => {
+        res.status(200).send('NEXT CALLED');
+      });
+      app.locals.spotify = {
+        getApi: () => Promise.resolve({
+          getAlbum: () => ({}),
+        }),
+      };
+      this.request = Request(app);
+      this.request.get('/data/album/1').expect('NEXT CALLED', done);
     });
-    app.locals.spotify = {
-      getApi: () => Promise.resolve({
-        getAlbum: () => ({}),
-      }),
-    };
-    const request = Request(app);
-    request.get('/data/album/1').expect('NEXT CALLED', done);
   });
 });
