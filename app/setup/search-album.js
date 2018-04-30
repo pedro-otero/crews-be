573a63a3c10e4f3192a99a46aace32f5eef1b511
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-30 18:27:11 -0400

    Move all custom setup to a specific module

--- app/setup/search-album.js
+++ app/setup/search-album.js
@@ -47,4 +47,32 @@ const loggerFactory = ({ id, artists: [{ name: artist }], name }) => createLogge
   ],
 });
 
-module.exports = search(discogs, loggerFactory);
+const SpotifyWebApi = require('spotify-web-api-node');
+const { store, actions } = require('../src/redux/state');
+const Query = require('../src/redux/view/query');
+
+const query = require('../src/routes/album/query');
+const searchRoute = require('../src/routes/album/search');
+const spotify = require('../src/routes/album/spotify');
+
+const SpotifyWrapper = require('../src/api/spotify');
+
+module.exports = (app) => {
+  /* App locals setup */
+  app.locals = {
+    store,
+    actions,
+    searchAlbum: search(discogs, loggerFactory),
+    Query,
+    spotify: SpotifyWrapper(SpotifyWebApi),
+  };
+
+  // Routes setup
+  // 1. Fetches album from Spotify
+  app.use('/data/album', spotify);
+  // 2. Starts a earch if there isn't one already
+  app.use('/data/album', searchRoute);
+  // 3. Queries the store for data about an album and sends it to client
+  app.use('/data/album', query);
+};
+
--- index.js
+++ index.js
@@ -14,21 +14,10 @@ const path = require('path');
 const favicon = require('static-favicon');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const setup = require('./app/setup/search-album');
 
 const routes = require('./routes/index');
 const users = require('./routes/users');
-
-// Custom imports
-const SpotifyWebApi = require('spotify-web-api-node');
-const { store, actions } = require('./app/src/redux/state');
-const Query = require('./app/src/redux/view/query');
-
-const query = require('./app/src/routes/album/query');
-const search = require('./app/src/routes/album/search');
-const spotify = require('./app/src/routes/album/spotify');
-const searchAlbum = require('./app/setup/search-album');
-
-const SpotifyWrapper = require('./app/src/api/spotify');
 
 const app = express();
 
@@ -43,25 +32,10 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* App locals setup */
-app.locals = {
-  store,
-  actions,
-  searchAlbum,
-  Query,
-  spotify: SpotifyWrapper(SpotifyWebApi),
-};
-
 app.use('/', routes);
 app.use('/users', users);
 
-// Routes setup
-// 1. Fetches album from Spotify
-app.use('/data/album', spotify);
-// 2. Starts a earch if there isn't one already
-app.use('/data/album', search);
-// 3. Queries the store for data about an album and sends it to client
-app.use('/data/album', query);
+setup(app);
 
 // / catch 404 and forwarding to error handler
 app.use((req, res, next) => {
