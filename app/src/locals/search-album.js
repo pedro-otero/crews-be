45c9d0df95d255bb14c2f2cde53830b826055a1a
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-30 14:57:58 -0400

    Use winston timestamp format

--- app/src/locals/search-album.js
+++ app/src/locals/search-album.js
@@ -3,13 +3,15 @@ const winston = require('winston');
 const discogs = require('../api/discogs');
 const search = require('../search');
 
+const { combine, printf, timestamp } = winston.format;
+
 const createLogger = ({ id }) => winston.createLogger({
   levels: {
     error: 0,
     info: 1,
     debug: 2,
   },
-  format: winston.format.simple(),
+  format: combine(timestamp(), printf(info => `${info.timestamp} ${info.message}`)),
   transports: [
     new winston.transports.Console({ level: 'info' }),
     new winston.transports.File({ filename: `app/log/${id}.log`, level: 'debug' }),
