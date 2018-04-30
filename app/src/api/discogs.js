cd3b379925ba7f3c72b685fe164ae16cde038750
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-30 15:51:26 -0400

    Put discogs setup into the search setup module

--- app/src/api/discogs.js
+++
@@ -1,9 +0,0 @@
-const Throxy = require('throxy');
-const Disconnect = require('disconnect');
-
-const { agent, keys } = require('../../../disconnect-config.json');
-
-const disconnect = new Disconnect.Client(agent, keys);
-const throttledDatabase = new Throxy(disconnect.database(), 1100);
-
-module.exports = { db: throttledDatabase, PAUSE_NEEDED_AFTER_429: 30000 };
--- app/src/locals/search-album.js
+++ app/src/locals/search-album.js
@@ -1,6 +1,16 @@
 const winston = require('winston');
+const Throxy = require('throxy');
+const Disconnect = require('disconnect');
 
-const discogs = require('../api/discogs');
+const { agent, keys } = require('../../../disconnect-config.json');
+
+const discogs = {
+  db: new Throxy(
+    new Disconnect.Client(agent, keys).database(),
+    1100
+  ),
+  PAUSE_NEEDED_AFTER_429: 30000,
+};
 const search = require('../search');
 
 const { combine, printf, timestamp } = winston.format;
@@ -11,7 +21,10 @@ const createLogger = ({ id, artists: [{ name: artist }], name }) => winston.crea
     info: 1,
     debug: 2,
   },
-  format: combine(timestamp(), printf(info => `${info.timestamp} ${artist} - ${name} (${id}) :: ${info.message}`)),
+  format: combine(
+    timestamp(),
+    printf(info => `${info.timestamp} ${artist} - ${name} (${id}) :: ${info.message}`)
+  ),
   transports: [
     new winston.transports.Console({ level: 'info' }),
     new winston.transports.File({ filename: `app/log/${id}.log`, level: 'debug' }),
