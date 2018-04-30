1810d4426794950b98d0a57500683c3cd9e02f98
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-30 16:10:46 -0400

    Re arrange and comment

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const favicon = require('static-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const SpotifyWebApi = require('spotify-web-api-node');
-
-const { store, actions } = require('./app/src/redux/state');
-const Query = require('./app/src/redux/view/query');
 
 const routes = require('./routes/index');
 const users = require('./routes/users');
+
+const SpotifyWebApi = require('spotify-web-api-node');
+const { store, actions } = require('./app/src/redux/state');
+const Query = require('./app/src/redux/view/query');
+
 const query = require('./app/src/routes/album/query');
 const search = require('./app/src/routes/album/search');
 const spotify = require('./app/src/routes/album/spotify');
+const searchAlbum = require('./app/src/locals/search-album');
 
 const SpotifyWrapper = require('./app/src/api/spotify');
 
@@ -31,18 +33,23 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* App locals setup */
-
-app.locals.searchAlbum = require('./app/src/locals/search-album');
-
-app.locals.Query = Query;
-app.locals.store = store;
-app.locals.actions = actions;
-app.locals.spotify = SpotifyWrapper(SpotifyWebApi);
+app.locals = {
+  store,
+  actions,
+  searchAlbum,
+  Query,
+  spotify: SpotifyWrapper(SpotifyWebApi),
+};
 
 app.use('/', routes);
 app.use('/users', users);
+
+// Routes setup
+// 1. Fetches album from Spotify
 app.use('/data/album', spotify);
+// 2. Starts a earch if there isn't one already
 app.use('/data/album', search);
+// 3. Queries the store for data about an album and sends it to client
 app.use('/data/album', query);
 
 // / catch 404 and forwarding to error handler
