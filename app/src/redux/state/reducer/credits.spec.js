a5df7b52b2292fc3c6a4ea668cd0d343cdb36e8d
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-30 11:27:27 -0400

    Make sure items without accents issues make it to the list

--- app/src/redux/state/reducer/credits.spec.js
+++ app/src/redux/state/reducer/credits.spec.js
@@ -26,17 +26,21 @@ describe('Credit reducer', () => {
       name: 'Pe1', role: 'R1', track: 'T1',
     }, {
       name: 'Pé2', role: 'R2', track: 'T2',
+    }, {
+      name: 'P3', role: 'R3', track: 'T3',
     }], {
       type: ADD_CREDITS,
       credits: [{
         name: 'Pé1', role: 'R1', track: 'T1',
       }, {
         name: 'Pe2', role: 'R2', track: 'T2',
+      }, {
+        name: 'P4', role: 'R4', track: 'T4',
       }],
     });
 
     it('test length', () => {
-      assert.equal(credits.length, 2);
+      assert.equal(credits.length, 4);
     });
 
     describe('but prefers accented version', () => {
@@ -47,6 +51,14 @@ describe('Credit reducer', () => {
       it('of Re', () => {
         assert(!credits.find(c => c.name === 'P2' && c.role === 'Re2' && c.track === 'T2'));
       });
+    });
+
+    it('adds credits that have no equivalent accented or not', () => {
+      assert(credits.find(c => c.name === 'P3' && c.role === 'R3' && c.track === 'T3'));
+    });
+
+    it('keeps credits that have no equivalent accented or not', () => {
+      assert(credits.find(c => c.name === 'P4' && c.role === 'R4' && c.track === 'T4'));
     });
   });
 });
