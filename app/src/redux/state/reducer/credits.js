8056aeed571432132c8e9f16d65fb73b90473cd8
Pedro Otero <pedro.otero.prada@gmail.com>
2018-04-30 11:24:24 -0400

    Avoid duplicates keeping accented versions of names

--- app/src/redux/state/reducer/credits.js
+++ app/src/redux/state/reducer/credits.js
@@ -1,12 +1,28 @@
+const accents = require('remove-accents');
+
 const { ADD_CREDITS } = require('../action/constants');
 
 module.exports = (state = [], { type, credits }) => {
   switch (type) {
     case ADD_CREDITS:
-      return [
-        ...credits.filter(({ name, role, track }) =>
-          !state.find(s => s.name === name && s.role === role && s.track === track)),
-        ...state];
+      const unique = credits.filter(({ name, role, track }) =>
+        !state.find(s => s.name === name && s.role === role && s.track === track));
+      const merged = [...unique, ...state];
+      return merged.reduce((filtered, current) => {
+        if (accents.has(current.name)) {
+          const index = filtered.findIndex(f => f.name === accents.remove(current.name));
+          if (index > -1) {
+            return [current, ...filtered.filter((_, i) => i !== index)];
+          }
+        } else {
+          const index = filtered.findIndex(f => accents.remove(f.name) === current.name);
+          if (index === -1) {
+            return [current, ...filtered];
+          }
+          return filtered;
+        }
+        return [current, ...filtered];
+      }, []);
     default:
       return state;
   }
--- app/src/redux/state/reducer/credits.spec.js
+++ app/src/redux/state/reducer/credits.spec.js
@@ -9,7 +9,7 @@ describe('Credit reducer', () => {
   describe('adds credits', () => {
     const credits = reduce([], {
       type: ADD_CREDITS,
-      credits: ['value'],
+      credits: [{ name: 'x', role: 'y', track: 'z' }],
     });
 
     it('test length', () => {
@@ -17,26 +17,36 @@ describe('Credit reducer', () => {
     });
 
     it('test value', () => {
-      assert.equal(credits[0], 'value');
+      assert.equal(credits[0].name, 'x');
     });
   });
 
-  describe.skip('avoids duplicate credits', () => {
+  describe('avoids duplicate credits', () => {
     const credits = reduce([{
       name: 'Pe1', role: 'R1', track: 'T1',
+    }, {
+      name: 'Pé2', role: 'R2', track: 'T2',
     }], {
       type: ADD_CREDITS,
       credits: [{
         name: 'Pé1', role: 'R1', track: 'T1',
+      }, {
+        name: 'Pe2', role: 'R2', track: 'T2',
       }],
     });
 
     it('test length', () => {
-      assert.equal(credits.length, 1);
+      assert.equal(credits.length, 2);
     });
 
-    it('but prefers accented version', () => {
-      assert.equal(credits.length, 1);
+    describe('but prefers accented version', () => {
+      it('of Pe', () => {
+        assert(!credits.find(c => c.name === 'Pe1' && c.role === 'R1' && c.track === 'T1'));
+      });
+
+      it('of Re', () => {
+        assert(!credits.find(c => c.name === 'P2' && c.role === 'Re2' && c.track === 'T2'));
+      });
     });
   });
 });
