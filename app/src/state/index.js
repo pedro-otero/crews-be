f40938387d80302d11517efbc2f03729499f00e3
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-25 20:12:53 -0400

    Add merge code to state module

--- app/src/state/index.js
+++ app/src/state/index.js
@@ -1,4 +1,30 @@
-const reduceCredits = require('./credits/merge');
+const accents = require('remove-accents');
+
+const hasAccentedName = c => accents.has(c.name);
+
+const reduceCredits = (state = [], credits) => credits
+  .filter(c => !hasAccentedName(c))
+  .concat(state.filter(c => !hasAccentedName(c)))
+  .reduce((all, current) => {
+    if (all.find(item =>
+      accents.remove(item.name) === current.name &&
+        item.role === current.role &&
+        item.track === current.track)) {
+      return all;
+    }
+    return all.concat([current]);
+  }, credits
+    .filter(hasAccentedName)
+    .concat(state.filter(hasAccentedName)))
+  .reduce((all, current) => {
+    if (all.find(item =>
+      item.name === current.name &&
+        item.role === current.role &&
+        item.track === current.track)) {
+      return all;
+    }
+    return all.concat([current]);
+  }, []);
 
 const albums = [];
 let searches = [];
